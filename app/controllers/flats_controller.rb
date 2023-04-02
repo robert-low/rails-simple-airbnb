d903f95e7675af674c65b093c7e7e44c04859bb2
Robert Low <robert.low91@gmail.com>
2023-04-02 18:28:28 +0100

    edit and show

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,4 +1,6 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
   def index
     @flats = Flat.all
   end
@@ -16,9 +18,32 @@ class FlatsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @flat.update(flat_params)
+      redirect_to flat_path(@flat)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
+  end
+
   private
 
   def flat_params
     params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+
+  def set_flat
+    @flat = Flat.find(params[:id])
   end
 end
