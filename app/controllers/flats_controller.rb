b0d9312f664f8b34c47c5f59dd403b1f3ee35308
Robert Low <robert.low91@gmail.com>
2023-04-02 17:54:49 +0100

    add a flat

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -6,4 +6,19 @@ class FlatsController < ApplicationController
   def new
     @flat = Flat.new
   end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flats_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,5 +1,5 @@
 class Flat < ApplicationRecord
-  validates :name, :address, :price, presence: true
+  validates :name, :address, :price_per_night, presence: true
   validates :number_of_guests, length: { minimum: 1 }
   validates :description, length: {
     maximum: 200,
