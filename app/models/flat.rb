d8ec27b92ae06234b6c5f7974398506974c1da63
Robert Low <robert.low91@gmail.com>
2023-04-02 17:42:20 +0100

    flat model validations

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,8 @@
 class Flat < ApplicationRecord
+  validates :name, :address, :price, presence: true
+  validates :number_of_guests, length: { minimum: 1 }
+  validates :description, length: {
+    maximum: 200,
+    too_long: '200 characters is the maximum allowed'
+  }
 end
